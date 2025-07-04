a44a821ab1fa811bf007a48fe9fb8dccda357607
prasoonk1204 <prasoonkumar467@gmail.com>
2025-07-04 19:09:38 +0530

    Implement authentication features including login page, user authentication, and session management

--- nextjs-dashboard/app/dashboard/customers/page.tsx
+++ nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -1,3 +1,12 @@
+import CustomersTable from "@/app/ui/customers/table";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Customers",
+};
+
 export default function Page() {
-  return <p>Customers Page</p>;
+  return <>
+    <CustomersTable customers={[]} />
+  </>
 }
